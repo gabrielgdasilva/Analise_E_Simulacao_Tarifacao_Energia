03438fa26e818b3196ad7779200b2568a8780a03
Gabriel Silva <20029646@anhembimorumbi.edu.br>
2016-04-25 22:10:34 -0300

    Inicio do Projeto Site do TCC

---
+++ Analise_E_Simulacao_Tarifacao_Energia/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Analise_E_Simulacao_Tarifacao_Energia
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Analise_E_Simulacao_Tarifacao_Energia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Analise_E_Simulacao_Tarifacao_Energia.MvcApplication" Language="C#" %>
